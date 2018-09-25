59ccbf70482e7e27264b8663802ebc159fd41f08
Dmitrii Mostovshchikov <dmadm2008@gmail.com>
2018-09-26 00:39:01 +0300

    Updated mk inventory script

--- openshift-multimaster/scripts/ocp3_mk_inventory.sh
+++ openshift-multimaster/scripts/ocp3_mk_inventory.sh
@@ -19,29 +19,43 @@ cat << INVENTORY
 all:
     vars:
         ansible_ssh_user: centos
-        ansible_private_key_file: "key"
-        ansible_become: True
-        openshift_deployment_type: origin
+        ansible_become: true
+        private_domain: 'ocp.local'
+        public_domain: 'ocp.litf4'
+        ansible_private_key_file: 'key'
         domain_name: novalocal
-        loadbalancer_fqdn: 'openshift.{{ domain_name }}'
-        loadbalancer_internal_fqdn: openshift-internal.{{ domain_name }}'
+#        loadbalancer_fqdn: 'openshift.{{ domain_name }}'
+#        loadbalancer_internal_fqdn: openshift-internal.{{ domain_name }}'
     children:
-        masters: { children: { master_nodes: }}
-        etcd: { children: { master_nodes: }}
-        glusterfs: { children: { storage_nodes: }}
-        glusterfs_registry: { children: { storage_nodes: }}
-        OSEv3:
+         local: { hosts: { 127.0.0.1: }}
+         etcd: { children: { masters: }}
+         glusterfs_registry: { children: { glusterfs: }}
+         lb: { children: { infra: }}
+         OSEv3:
             children:
-                master_nodes:
-                app_nodes:
+                masters:
+                nodes:
                 etcd:
+                lb:
+                local:
                 glusterfs:
                 glusterfs_registry:
             vars:
-                openshift_master_cluster_hostname: "{{ loadbalancer_internal_fqdn }}"
-                openshift_master_cluster_public_hostname: "{{ loadbalancer_fqdn }}"
-                ansible_user: centos
+                openshift_master_cluster_method: native
+                openshift_master_cluster_hostname: '{{ private_domain }}'
+                openshift_master_cluster_public_hostname: '{{ public_domain }}'
+                openshift_master_default_subdomain: 'apps.{{ public_domain }}'
+                openshift_master_cluter_ip: '$( get_value cluster_private_ip )'
+                openshift_master_cluster_public_ip: '$( get_value cluster_public_ip )'
+                # openshift_master_portal_net: '$( get_value portal_net_cidr )'
                 openshift_deployment_type: origin
+                os_sdn_network_plugin_name: 'redhat/openshift-ovs-multitenant'
+                openshift_hosted_registry_storage_kind: glusterfs
+                openshift_hosted_registry_storage_volume_size: 50Gi
+                openshift_storage_glusterfs_registry_storageclass: True
+                local_dns: '$( get_value local_dns_ip )'
+                external_interface: bond0
+
                 openshift_disable_check:
                   - docker_image_availability
                   - memory_availability
@@ -66,7 +80,7 @@ all:
                 openshift_storage_glusterfs_registry_block_storageclass: true
                 openshift_storage_glusterfs_registry_block_storageclass_default: false
                 openshift_storage_glusterfs_registry_block_host_vol_size: 20
-                openshift_master_ldap_ca_file: '/root/deploy/openshift-ansible/freeipa-cacert.p12'
+                openshift_master_ldap_ca_file: '/tmp/freeipa-cacert.p12'
                 openshift_master_identity_providers:
                   - name: FreeIPA
                     challenge: true
@@ -83,60 +97,50 @@ all:
                     # ca: freeipa-cacert.p12
                     url: 'ldap://freeipa.labcloud.litf4:389/cn=users,cn=accounts,dc=labcloud,dc=litf4?uid?sub?(&(!(memberOf=cn=disabledusers,cn=groups,cn=accounts,dc=labcloud,dc=litf4))(|(memberOf=cn=openshiftusers,cn=groups,cn=accounts,dc=labcloud,dc=litf4)(memberOf=cn=openshiftadmins,cn=groups,cn=accounts,dc=labcloud,dc=litf4)))'
 
-
-        dns_servers:
-            # hosts:
-            #     dns_server: { ansible_host: $( get_value dnsserver_ip1 )}
-            children:
-                management:
-            vars:
-                lbfqdn_ip: $( get_value cluster_vip_ip1 )
-        loadbalancer_nodes:
-            children:
-                masters:
-        management:
-            hosts:
-                buildhost: { ansible_host: $( get_value buildhost_ip1 )}
-        lb:
-            children:
-                loadbalancer_nodes:
-        app_nodes:
-            hosts:
-                node1: { ansible_host: $( get_value appnode1_ip1 )}
-                node2: { ansible_host: $( get_value appnode2_ip1 )}
-                node3: { ansible_host: $( get_value appnode3_ip1 )}
-            vars:
-                openshift_node_group_name: "node-config-compute"
-            children:
-                storage_nodes:
-        master_nodes:
-            hosts:
-                master1: { ansible_host: $( get_value master1_ip1 )}
-                master2: { ansible_host: $( get_value master2_ip1 )}
-                master3: { ansible_host: $( get_value master3_ip1 )}
-            vars:
-                openshift_node_group_name: "node-config-master"
-                openshift_schedulable: True
-        infra_nodes:
-            hosts:
-                infra1: { ansible_host: $( get_value infra1_ip1 )}
-                infra2: { ansible_host: $( get_value infra2_ip1 )}
-                infra3: { ansible_host: $( get_value infra3_ip1 )}
-            vars:
-                openshift_node_group_name: "node-config-infra"
-        nodes:
-            children:
-                app_nodes:
-                infra_nodes:
-                master_nodes:
-        storage_nodes:
-            hosts:
-                storage1: { ansible_host: $( get_value storagenode1_ip1 )}
-                storage2: { ansible_host: $( get_value storagenode2_ip1 )}
-                storage3: { ansible_host: $( get_value storagenode3_ip1 )}
-            vars:
-                glusterfs_devices: [ "/dev/vdc", "/dev/vde", "/dev/vdd" ]
-                openshift_node_group_name: "node-config-infra"
+         masters:
+             hosts:
+                master1: { ansible_host: $( get_value master1_ip1 ), openshift_ip: $( get_value master1_ip2 ), openshift_hostname: 'master1.{{ private_domain }}' }
+                master2: { ansible_host: $( get_value master2_ip2 ), openshift_ip: $( get_value master2_ip2 ), openshift_hostname: 'master2.{{ private_domain }}' }
+                master3: { ansible_host: $( get_value master3_ip3 ), openshift_ip: $( get_value master3_ip2 ), openshift_hostname: 'master3.{{ private_domain }}' }
+             vars:
+                 containerized: true
+                 openshift_schedulable: false
+                 openshift_node_group_name: 'node-config-master'
+         infra:
+             hosts:
+                infra1: { ansible_host: $( get_value infra1_ip1 ), openshift_public_ip: $( get_value infra1_ip1 ), openshift_ip: $( get_value infra1_ip2 ), openshift_hostname: 'infra1.{{ private_domain }}' }
+                infra2: { ansible_host: $( get_value infra2_ip2 ), openshift_public_ip: $( get_value infra2_ip2 ), openshift_ip: $( get_value infra2_ip2 ), openshift_hostname: 'infra2.{{ private_domain }}' }
+                infra3: { ansible_host: $( get_value infra3_ip3 ), openshift_public_ip: $( get_value infra3_ip3 ), openshift_ip: $( get_value infra3_ip2 ), openshift_hostname: 'infra3.{{ private_domain }}' }
+             vars:
+                 containerized: true
+                 openshift_schedulable: true
+                 openshift_node_group_name: 'node-config-infra'
+         app:
+             hosts:
+                app1: { ansible_host: $( get_value app1_ip1 ), openshift_ip: $( get_value app1_ip2 ), openshift_hostname: 'app1.{{ private_domain }}' }
+                app2: { ansible_host: $( get_value app2_ip1 ), openshift_ip: $( get_value app2_ip2 ), openshift_hostname: 'app2.{{ private_domain }}' }
+                app3: { ansible_host: $( get_value app3_ip1 ), openshift_ip: $( get_value app3_ip2 ), openshift_hostname: 'app3.{{ private_domain }}' }
+             vars:
+                 containerized: true
+                 openshift_schedulable: true
+                 openshift_node_group_name: 'node-config-compute'
+         glusterfs:
+             children: { app: }
+             hosts:
+                storage1: { ansible_host: $( get_value storage1_ip1 ), glusterfs_ip: $( get_value storage1_ip2 ), openshift_hostname: 'storage1.{{ private_domain }}' }
+                storage2: { ansible_host: $( get_value storage2_ip1 ), glusterfs_ip: $( get_value storage2_ip2 ), openshift_hostname: 'storage2.{{ private_domain }}' }
+                storage3: { ansible_host: $( get_value storage3_ip1 ), glusterfs_ip: $( get_value storage3_ip2 ), openshift_hostname: 'storage3.{{ private_domain }}' }
+             vars:
+                 containerized: true
+                 openshift_schedulable: true
+                 openshift_node_group_name: 'node-config-infra'
+                 glusterfs_devices: [ "/dev/vdc", "/dev/vde", "/dev/vdd" ]
+         nodes:
+             children:
+                 masters:
+                 infra:
+                 app:
+                 glusterfs:
 
 INVENTORY
 
