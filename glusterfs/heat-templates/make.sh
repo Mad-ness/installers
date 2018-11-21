e6438a04de54ea08cf2269f7730469cbf0d5351e
Dmitrii Mostovshchikov <dmadm2008@gmail.com>
2018-11-22 00:24:37 +0300

    Updated template

--- glusterfs/heat-templates/make.sh
+++ glusterfs/heat-templates/make.sh
@@ -1,2 +1,2 @@
-./gen_template.py -t glustefs.yaml  -e backend_network=ipsystems-management -e frontend_network=OSQ-External-Network -e dmitrii-key1 -e volume_size=20 -e num_volumes=4 -e num_servers=5 -o output.yml
+./gen_template.py -t glustefs.yaml  -e backend_network=ipsystems-management -e frontend_network=OSQ-External-Network -e sshkey=dmitrii-key1 -e volume_size=20 -e num_volumes=4 -e num_servers=5 -o output.yml
 
